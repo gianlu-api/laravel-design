144e18f050f4ad35c0c73e1c49184899596c5244
gianluca <gianluca.api96@gmail.com>
2024-11-18 22:48:18 +0100

    React make command

---
+++ src/Commands/Console/ReactMakeCommand.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace gianluApi\laravelDesign\Commands\Console;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use InvalidArgumentException;
+
+class ReactMakeCommand extends GeneratorCommand
+{
+
+    protected $signature = 'make:react {name} {namespace?}';
+    protected $description = 'Create new react file';
+    protected $type = 'React';
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/../../../stubs/React.stub';
+    }
+
+    /**
+     * @param $name
+     * @return string
+     */
+    protected function getPath($name): string
+    {
+        $path = $this->argument('namespace') ?? resource_path('js/Pages');
+
+        if (!is_string($path)) {
+            throw new InvalidArgumentException("namespace must be a string");
+        }
+
+        $name = $this->argument('name');
+
+        if (!is_string($name)) {
+            throw new InvalidArgumentException("name must be a string");
+        }
+
+        return resource_path("$path/$name.jsx");
+    }
+
+    /**
+     * @param $name
+     * @return string
+     * @throws FileNotFoundException
+     */
+    protected function buildClass($name): string
+    {
+        $stub = $this->files->get($this->getStub());
+
+        $name = $this->argument('name');
+
+        if (!is_string($name)) {
+            throw new InvalidArgumentException("name must be a string");
+        }
+
+        return str_replace('{{ name }}', $name, $stub);
+    }
+
+}
