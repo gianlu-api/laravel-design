afba59de150889b9b589894f43b6498d45f6f26f
gianluca <gianluca.api96@gmail.com>
2024-11-18 22:50:00 +0100

    laravel configurations

---
+++ config/laravel-design.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'migration' => [
+        'tables' => [
+
+        ],
+    ],
+    'classes' => [
+        [
+
+        ]
+    ],
+    'interfaces' => [
+        [
+
+        ]
+    ],
+    'abstract_classes' => [
+        [
+
+        ]
+    ],
+    'requests' => [
+        [
+
+        ]
+    ],
+    'middlewares' => [
+        [
+
+        ]
+    ],
+    'resources' => [
+        [
+
+        ]
+    ],
+    'controllers' => [
+        [
+
+        ]
+    ],
+    'views' => [
+        [
+
+        ]
+    ],
+    'directories' => [
+        [
+
+        ]
+    ]
+];
---
+++ src/Providers/LaravelDesignServiceProvider.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace gianluApi\laravelDesign\Providers;
+
+use gianluApi\laravelDesign\Commands\Console\AbstractClassMakeCommand;
+use gianluApi\laravelDesign\Commands\Console\ClassMakeCommand;
+use gianluApi\laravelDesign\Commands\Console\DesignMakeCommand;
+use gianluApi\laravelDesign\Commands\Console\InterfaceMakeCommand;
+use gianluApi\laravelDesign\Commands\Console\ReactMakeCommand;
+use gianluApi\laravelDesign\Commands\Console\VueMakeCommand;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDesignServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishes([__DIR__ . '/../../config/laravel-design.php' => config_path('laravel-design.php')], 'gianlu-api:laravel-design:config');
+
+        $this->commands([
+            DesignMakeCommand::class,
+            ClassMakeCommand::class,
+            InterfaceMakeCommand::class,
+            AbstractClassMakeCommand::class,
+            VueMakeCommand::class,
+            ReactMakeCommand::class
+        ]);
+    }
+
+    public function register()
+    {
+
+    }
+}
